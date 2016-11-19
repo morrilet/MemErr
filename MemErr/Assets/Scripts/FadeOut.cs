eeee424ba415629cfe45529641e72391a49fee9a
Ethan Morrill-Ploum <findwaldoFTLOG@gmail.com>
2016-11-18 16:43:04 -0800

    Dedicated Version
    I added a fading bit of text when the house scene loads that contains a
    dedication to Jasen and Gus. This was done upon Jeremy's request because
    he simply wanted to fuck with Jasen. I kind of had to. The text is
    turned off in the game, but I built it with it enabled once for Jeremy.

---
+++ MemErr/Assets/Scripts/FadeOut.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FadeOut : MonoBehaviour 
+{
+	Text txtObj;
+	float fadeTimer = 0f;
+	float fadeTime = 7.5f;
+
+	void Start () 
+	{
+		txtObj = GetComponent<Text> ();
+	}
+
+	void Update ()
+	{
+		Color color = txtObj.color;
+		color.a = Mathf.Lerp (255, 0, fadeTimer / fadeTime) / 255f;
+		txtObj.color = color;
+		Debug.Log (txtObj.color.a);
+
+		if (fadeTimer >= fadeTime)
+		{
+			Destroy (this.gameObject);
+		}
+
+		fadeTimer += Time.deltaTime;
+	}
+}
